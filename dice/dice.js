6bfd25526f112ae872742faacc6bbc2ad254ecf3
MIGELZO <154762117+MIGELZO@users.noreply.github.com>
2024-02-16 20:12:08 +0200

    update1

--- dice/dice.js
+++ dice/dice.js
@@ -1,16 +1,36 @@
-const dice = document.getElementById('dice-btn');
+window.addEventListener("load", () => {
+  const dice = document.getElementById("dice-btn");
+  dice.addEventListener("click", intervalFunc);
 
-function diceGame(){
-//dice 1 
-let randomDice = Math.floor((Math.random()*6)+1);
-let diceLocation = "./assetts/img/" + "number" + randomDice+ ".png";
-let img1 = document.querySelectorAll("img")[0];
-img1.setAttribute("src",diceLocation);
+  timer = 30;
+  intervalId1;
+  intervalId2;
+  p1Result = 0;
+  p2Result = 0;
 
-let randomDice2 = Math.floor((Math.random()*6)+1);
-let diceLocation2 = "./assetts/img/" + "number" + randomDice2+ ".png";
-let img2 = document.querySelectorAll("img")[1];
-img2.setAttribute("src",diceLocation2);
+    function intervalFunc() {
+        intervalId1 = setInterval(player1Turn , 100)
+    }
 
+  function player1Turn() {
 
-}
+    let randomDice = Math.floor(Math.random() * 6 + 1);
+    let diceLocation = "accets/number" + randomDice + ".png";
+    console.log(diceLocation);
+    let img1 = document.querySelectorAll("img")[0];
+    img1.setAttribute("src", diceLocation);
+    
+    let randomDice2 = Math.floor(Math.random() * 6 + 1);
+    let diceLocation2 = "accets/number" + randomDice2 + ".png";
+    let img2 = document.querySelectorAll("img")[1];
+    img2.setAttribute("src", diceLocation2);
+    
+
+    timer--;
+    if (timer == 0) {
+        clearInterval(intervalId1)
+    }
+    
+
+  }
+});
