c6b765ae3db7d02586755576ab25a9c092599d07
MIGELZO <154762117+MIGELZO@users.noreply.github.com>
2024-02-23 21:50:25 +0200

    shishi 21.50

---
+++ Async&await/app.js
@@ -0,0 +1,59 @@
+async function getCountries() {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    console.log(data);
+    showCountriesOnDocument(data);
+  }
+  
+   function showCountriesOnDocument(countries) {
+    let tableBody = document.getElementById("tableBody");
+    countries.forEach((country) => {
+      // country name
+      let newTr = document.createElement("tr");
+      let nameTd = document.createElement("td");
+      nameTd.innerText = country.name.common;
+      newTr.appendChild(nameTd);
+      tableBody.appendChild(newTr);
+      
+      // country img
+      let imgTd = document.createElement("td");
+      let newImg = document.createElement("img");
+      newImg.src = country.flags.png;
+      imgTd.appendChild(newImg);
+      newTr.appendChild(imgTd);
+      
+      // country location
+      let locationTd = document.createElement("td");
+      let mapLink = document.createElement("a");
+      mapLink.href = country.maps.googleMaps;
+      mapLink.setAttribute("target", "_blank");
+      mapLink.innerText = `${country.name.common} on map`
+      locationTd.appendChild(mapLink);
+      newTr.appendChild(locationTd);
+
+      // country Continent
+      let continentTd = document.createElement("td");
+      continentTd.innerText = country.continents[0];
+      newTr.appendChild(continentTd);
+
+      // country Continent
+      let currenciesTd = document.createElement("td");
+      let currencieShortName = Object.keys(country.currencies)[0]
+      let currencieName = country.currencies[currencieShortName].name
+      let currencieSymbol = country.currencies[currencieShortName].symbol
+      currenciesTd.innerText = `${currencieName} - ${currencieSymbol}`;
+      newTr.appendChild(currenciesTd);
+
+
+    });
+  }
+  
+  getCountries();
+
+//ש.ב
+//לחקור את המערך של האובייקטים
+//להציג טבלה בעמוד
+//טבלה עם נתונים על כל המדינות
+
+//עמודות בטבלה
+//שם המדינה, תמונה של הדגל, קישור לגוגל מפות, יבשת, מטבעות
