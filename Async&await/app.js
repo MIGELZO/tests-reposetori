337d0fc56b7e1838c1fb1413180140864ebffe77
MIGELZO <154762117+MIGELZO@users.noreply.github.com>
2024-02-23 22:03:56 +0200

    shishi 22.03

--- Async&await/app.js
+++ Async&await/app.js
@@ -1,7 +1,6 @@
 async function getCountries() {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const data = await response.json();
-    console.log(data);
     showCountriesOnDocument(data);
   }
   
